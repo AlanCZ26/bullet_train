a1954bc168c9b641eaed8c66a5e2ab4913250ceb
AlanCZ26 <alan.chen26@stgeorges.bc.ca>
2025-01-23 23:09:43 -0800

    clarity and math updates

--- bullet_train.py
+++ bullet_train.py
@@ -7,38 +7,67 @@ import numpy as np
 
 #1322 bullets
 
-total_ammo = 600
+mass_ammo = 600
 mass_bullet = 0.453592
-v_train = 0
-mass = 10000 + total_ammo
-time = 0
+velo_train = 0
+mass_train= 10000 + mass_ammo
+
 fired = 0
-x = []
-y = []
-v_p = v_train
 
-while (total_ammo - mass_bullet > 0):
-    fired += 1
-    mass -= mass_bullet
-    v_train += 1010 * mass_bullet / mass
-    total_ammo -= mass_bullet
-    print("ammo left: " + str(total_ammo) + ", v: " + str(v_train) + " / Fired bullets: " + str(fired) + " / Time taken: " + str(fired / 60) + " / accel: " + str((v_train - v_p) / (1/60)))
-    y.append(v_train)  
-    x.append(fired / 60)
-    v_p = v_train
+velo_prev = velo_train
+
+printer = []
+
+time_list = []
+velo_list = []
+accel_list = []
+
+sum_work = 0
 
 
-# make the data
+while (mass_ammo - mass_bullet > 0):
+    fired += 1 # increment bullets shot
+    mass_train-= mass_bullet # decrement train mass_trainby how much the bullet is firing to split the two into different objects before the calculation
+    velo_train += 1010 * mass_bullet / mass_train
+    mass_ammo -= mass_bullet
+    accel_cur= (velo_train - velo_prev) / (1/60)
+    velo_prev = velo_train
+
+    force_cur = mass_train * accel_cur
+    sum_work += force_cur * velo_train * 1/60
+    
+    printer.append("ammo left: " + str(round(mass_ammo, 2)) + "kg, v: " + str(round(velo_train, 5)) + "m/s, Fired: " + str(fired) + " bullets / Time: " + str(round(fired / 60, 5)) + " / accel: " + str(round(accel_cur, 5)))
+    
+    time_list.append(fired/60)
+    velo_list.append(velo_train)
+    accel_list.append(accel_cur)
+
+    
+
+print(printer[0])
+print(printer[1])
+print(printer[-1])
+print(sum_work)
 
 
-# size and color:
-sizes = 30
-colors = [30] * fired
+#formatting borrowed from Ryan
 
-# plot
-fig, ax = plt.subplots()
+plt.figure(figsize=(12, 6))
+plt.subplot(2, 1, 1)
+plt.plot(time_list, velo_list, label="Velocity vs Time", color="blue", linewidth=2)
+plt.title("Time vs Velocity", fontsize=16)
+plt.xlabel("Time (seconds)", fontsize=14)
+plt.ylabel("Velocity (m/s)", fontsize=14)
+plt.grid(True, linestyle="--", alpha=0.6)
+plt.legend(fontsize=12)
 
-ax.scatter(x, y, sizes, colors, vmin=0, vmax=100)
+plt.subplot(2, 1, 2)
+plt.plot(time_list, accel_list, label="Acceleration vs Time", color="red", linewidth=2)
+plt.title("Time vs Acceleration", fontsize=16)
+plt.xlabel("Time (seconds)", fontsize=14)
+plt.ylabel("Acceleration (m/s²)", fontsize=14)
+plt.grid(True, linestyle="--", alpha=0.6)
+plt.legend(fontsize=12)
 
-
+plt.tight_layout()
 plt.show()
